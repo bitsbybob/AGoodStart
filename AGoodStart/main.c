5cda4187b5f831af7adf94e3c069c41fa219e5c0
Bob Kolk <bobkolk@ipad.gateway.2wire.net>
2013-03-05 16:01:24 -0500

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Bob Kolk on 3/5/13.
+//  Copyright (c) 2013 Bob Kolk. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
